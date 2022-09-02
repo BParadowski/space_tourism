c36c75d8d02f05e03ba18ae86e4a4447b6ee854f
BParadowski <bartoszparadowski01@gmail.com>
2022-09-02 23:27:17 +0200

    Move images to public folder for links from json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      Hello world
+      <img src="/images/destination/image-moon.png" alt="" />
+    </div>
+  );
 }
 
 export default Home;
