c49b15034516e369dc8454dce2ca885761c1dac7
BParadowski <bartoszparadowski01@gmail.com>
2022-09-03 10:18:20 +0200

    Add destination component

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,27 @@ import Home from "./pages/Home";
 import Destination from "./pages/Destination";
 import Crew from "./pages/Crew";
 import Technology from "./pages/Technology";
+import data from "./assets/data.json";
 
 function App() {
+  const destinationData = data["destinations"];
+  const crewData = data["crew"];
+  const technology = data["technology"];
+
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/technology" element={<Technology />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/destination"
+            element={<Destination dataArr={destinationData} />}
+          />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Destination() {
-  return <div>Destination page</div>;
+interface DestinationData {
+  name: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  description: string;
+  distance: string;
+  travel: string;
+}
+
+function Destination(props: { dataArr: DestinationData[] }): JSX.Element {
+  const { dataArr } = props;
+  const [currentDestination, setCurrentDestination] = useState(0);
+  const { name, images, description, distance, travel } =
+    dataArr[currentDestination];
+
+  function switchDestination(index: number) {
+    setCurrentDestination(index);
+  }
+
+  const destinationNav = (
+    <ul>
+      {dataArr.map((destination, index) => (
+        <li key={destination.name} onClick={() => switchDestination(index)}>
+          {destination.name}
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      <h5>
+        <span>01</span> PICK YOUR DESTINATION
+      </h5>
+      <picture>
+        <source type="image/webp" srcSet={images.webp} />
+        <img src={images.png} alt="Picture of your chosen destination" />
+      </picture>
+      <nav>{destinationNav}</nav>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <h5>AVG. DISTANCE</h5>
+      <p className="subheading1">{distance}</p>
+      <h5>EST. TRAVEL TIME</h5>
+      <p className="subheading2">{travel}</p>
+    </div>
+  );
 }
 
 export default Destination;
