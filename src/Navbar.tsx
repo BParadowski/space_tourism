48ee98a19f6161e40e1ee2dd6c6f63c789c3e3b4
BParadowski <bartoszparadowski01@gmail.com>
2022-09-03 08:50:13 +0200

    Add home page markup

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,10 +7,28 @@ function Navbar() {
     <header>
       <img src={logo} alt="Space travel logo" />
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/destination">Destination</Link>
-        <Link to="/crew">Crew</Link>
-        <Link to="/technology">Technology</Link>
+        <ul>
+          <li>
+            <Link to="/">
+              <span>00</span> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/destination">
+              <span>01</span> Destination
+            </Link>
+          </li>
+          <li>
+            <Link to="/crew">
+              <span>02</span> Crew
+            </Link>
+          </li>
+          <li>
+            <Link to="/technology">
+              <span>03</span> Technology
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
